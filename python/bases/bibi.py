937980de0cc58ccbb36f72b8088c76a3fdcfd406
Cristiano <cristianochincarosanches@gmail.com>
2024-06-09 18:30:41 -0300

    binario--basic-functions--

--- python/bases/bibi.py
+++ python/bases/bibi.py
@@ -9,49 +9,95 @@ sys.path.append(d+'\\tp\\')
 from tempo import *
 #
 class bibi():
-    def __init__(self, number, index,ty) -> None:
-        self.number = number;self.ty = ty; self.index = index
+    time_init()
+    def __init__(self, number:'numero', index:'base1',index_2:'base2') -> 'Conversão':
+        self.number = number; self.index = index; self.index_2 = index_2
         #
         self.bi()
     def bi(self):
-        x = 0
-        alfa = {x:'A', x+1:'B',x+2:'C', x+3:'D', x+4:'E', x+5:'F', x+6:'G', x+7:'H', x+8:'I', x+9:'J', x+10:'K', x+11:'L', x+12:'M', x+13:'N', x+14:'O', x+15:'P', x+16:'Q', x+17:'R', x+18:'S', x+19:'T', x+20:'U', x+21:'V', x+22:'W', x+23:'X', x+24:'Y', x+25:'Z'}
-        n = self.number; ind = self.index
-        if self.ty == 'c':
-            lis_n = [[],[]]; alfa_i = 1
+        alfa = {10:'A', 11:'B',12:'C', 13:'D', 14:'E', 15:'F', 16:'G', 17:'H', 18:'I', 19:'J', 20:'K', 21:'L', 22:'M',23:'N', 24:'O', 25:'P', 26:'Q', 27:'R', 28:'S', 29:'T', 30:'U', 31:'V', 32:'W', 33:'X', 34:'Y', 35:'Z'}
+        alfa_ar = []; alfa_i = 1
+        n = self.number; ind = [self.index, self.index_2]
+        alfa_ar = [alfa]; lis_n = [[],[]]
+        if ind[0]>26:
+            alfa_ar.append({});letra = alfa.values(); e = 1; c = 36
             while True:
-                n = n/ind
-                if n - int(n) == 0: lis_n[1].append(0)
+                for i in letra:
+                    if c-26<ind[0]-26: alfa_ar[e][c] = i +'-'+str(alfa_i)
+                    c += 1
+                if c+10>ind[0]-26: break
+                e += 1; alfa_ar.append({})
+                alfa_i += 1
+        # print(alfa_ar)
+        #Conversão 1
+        lis_str = list(str(self.number)); e = [0,0]; cont = [0,0]; va = [0,1]; m = 0
+        for i in alfa_ar[len(alfa_ar)-1].values(): va[0]= i
+        va[0] = alfa_i; ponte = ''
+        while va[0]>10: va[0]/= 10; va[1] += 1
+        for i in lis_str:
+            if cont[0]>0: cont[0]-= 1
+            else: ponte = ''
+            if i == '-' or cont[0]>0:
+                if i == '-': ponte += lis_str[e[0]-1] + lis_str[e[0]]; lis_n[m].pop(e[1]);cont[0] = va[1]+1
+                elif i != '0':ponte += lis_str[e[0]]
+            elif i == '.':m = 1; 
+            else: lis_n[m].append(i)
+            if cont[0] == 1: lis_n[m].append(ponte)
+            e[0] += 1; e[1] = len(lis_n[m])-1
+        # print(lis_n, '1')
+        lis_n[0].reverse()
+        n = 0
+        for i in range(len(lis_n)):
+            if i == 0: v = 1
+            else: v = 0.1
+            for j in lis_n[i]:
+                try: j = int(j)
+                except:
+                    for k in alfa_ar:
+                        for l,m in k.items():
+                            if m == j: j = l; break
+                        if j == l: break
+                if j>ind[0]-1: print('Por favor, não coloque um número maior que a base'); break
+                elif j != 0: n += j*v
+                #
+                if i == 0: v *= ind[0]
+                else: v /= ind[0]
+        e = 0
+        # print(n, '2')
+        #Conversão 2
+        lis_n = [[int(n)], [n - int(n)]]
+        if lis_n[0] != 0:
+            n = lis_n[0][0]
+            while True:
+                n/=ind[1]
+                if n - int(n) == 0: lis_n[0].append(0)
                 else: 
-                    if int(n*ind - int(n)*ind)<10:
-                        lis_n[1].append(int(n*ind - int(n)*ind)); n = int(n)
+                    if int(n*ind[1] - int(n)*ind[1])<10: lis_n[0].append(int(n*ind[1] - int(n)*ind[1]))
                     else:
-                        while True:
-                            for j in alfa.keys():
-                                if j+10 == int(n*ind - int(n)*ind): lis_n[1].append(alfa[j]); break
-                            if j+10 == int(n*ind - int(n)*ind): break
-                            else: 
-                                x += 10
-                                alfa = {x:'A', x+1:'B',x+2:'C', x+3:'D', x+4:'E', x+5:'F', x+6:'G', x+7:'H', x+8:'I', x+9:'J', x+10:'K', x+11:'L', x+12:'M', x+13:'N', x+14:'O', x+15:'P', x+16:'Q', x+17:'R', x+18:'S', x+19:'T', x+20:'U', x+21:'V', x+22:'W', x+23:'X', x+24:'Y', x+25:'Z'}
-                                for j in alfa.keys():
-                                    alfa[j] += str(alfa_i)
-                                alfa_i += 1
+                        for i in alfa_ar:
+                            for j in i.keys():
+                                if j == int(n*ind[1] - int(n)*ind[1]): lis_n[0].append(i[j])
+                            if j == int(n*ind[1] - int(n)*ind[1]): break
                 if n<1: break
-            if self.number - int(self.number) != 0:
-                n = self.number - int(self.number)
-                while True:
-                    n *= ind
-                    lis_n[0].append(int(n))
-                    if (n - int(n))*ind == 0: break
-                    n = n - int(n)
-            lis_n[1].reverse();lis_n.reverse()
-        elif self.ty == 'd':
-            lis_n = [list(str(self.number)), 0]; lis_n[0].reverse()
-            v = 1
-            for i in lis_n[0]:
-                i = int(i)
-                if i>ind-1: print('Por favor, não coloque um número maior que a base'); break
-                elif i != 0: lis_n[1] += i*v
-                v *= ind
-        print(lis_n)
-bibi(10,16,'c')
+        #2
+        if lis_n[1] != 0:
+            n = lis_n[1][0]
+            while True:
+                n *= ind[1]
+                if int(n)<10:
+                    lis_n[1].append(int(n))
+                else:
+                    for i in alfa_ar:
+                        for j in i.keys():
+                            if j == int(n): lis_n[1].append(i[j]); break
+                        if j == int(n): break
+                if (n - int(n))*ind[1] == 0: break
+                n = n - int(n)
+        # print(lis_n)
+        lis_n[1].pop(0); lis_n[0].pop(0);lis_n[0].reverse()
+        for i in range(len(lis_n)): 
+            if i>0: print('.', end='')
+            for j in lis_n[i]: print(j, end='')
+        print('\r')
+        time_end()
+bibi('1.5',10,2)
--- python/bases/test.py
+++
@@ -1,3 +0,0 @@
-x = int(input())
-if x/5 - int(x/5)== 0: print('5')
-else: print('No')
